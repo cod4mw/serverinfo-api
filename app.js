af0461bae0a638d5ca4b8fdd1b34b639618ca929
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-07-17 13:22:01 +0000

    headers: allow CORS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var path = require('path');
+var cors = require('cors')
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
